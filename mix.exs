e9c301454b3993ea0aece1b049aaef0c6ede3f8d
matthew graham <mdg149@gmail.com>
2025-12-16 18:23:34 -0500

    bump to v0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rexult.MixProject do
   def project do
     [
       app: :rexult,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
